060fcefdb58b8e821010296d2febd03dfdc56239
Josh Quan <wrathofquan@gmail.com>
2021-10-20 16:43:41 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -2,6 +2,9 @@
 
 """ Module Description """
 
+# xmltotabular is designed for large, pseudo-xml. The nyt files need to be concatenated to be used efficiently in the program
+# In Bash: find ./nyt_nitf_*/*/ -type f -name "*.xml" -exec cat {} + > nyt_all_articles.xml
+
 import logging
 
 from xmltotabular import XmlCollectionToTabular
